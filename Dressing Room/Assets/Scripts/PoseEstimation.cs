6c6364da2a89e863ef221690ba48bb741d657df6
Loghin Vlad-Andrei <vlad.loghin00@gmail.com>
2020-12-18 07:59:10 +0200

    First Detection Parse

--- Dressing Room/Assets/Scripts/PoseEstimation.cs
+++ Dressing Room/Assets/Scripts/PoseEstimation.cs
@@ -1,14 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using TensorFlow;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using Google.Protobuf;
 
 public class PoseEstimation : MonoBehaviour
 {
     public int Width = 512;
     public int Height = 512;
     public int FPS = 30;
+
+    private Camera camera;
 
     WebCamTexture webcamTexture;
     Texture2D texture = null;
@@ -20,8 +26,38 @@ public class PoseEstimation : MonoBehaviour
     TFGraph graph;
     bool isPosing;
 
+    public GameObject clothMesh = null;
+
+    private static string getJointSuperior(string jointName)
+    {
+        switch (jointName)
+        {
+            case "leftEye" : return "none";
+            case "leftEar" : return "none";
+            case "leftShoulder" : return "leftEye";
+            case "leftElbow" : return "leftShoulder";
+            case "leftWrist" : return "leftElbow";
+            case "leftHip" : return "leftShoulder";
+            case "leftKnee" : return "leftHip";
+            case "leftAnkle" : return "leftKnee";
+            
+            case "rightEye" : return "none";
+            case "rightEar" : return "none";
+            case "rightShoulder" : return "rightEye";
+            case "rightElbow" : return "rightShoulder";
+            case "rightWrist" : return "rightElbow";
+            case "rightHip" : return "rightShoulder";
+            case "rightKnee" : return "rightHip";
+            case "rightAnkle" : return "rightKnee";
+            
+            default: return "none";
+        }
+    }
+
     void Start()
     {
+        this.camera = Camera.main;
+        
         WebCamDevice[] devices = WebCamTexture.devices;
         webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS);
         GetComponent<Renderer>().material.mainTexture = webcamTexture;
@@ -50,8 +86,117 @@ public class PoseEstimation : MonoBehaviour
         isPosing = true;
     }
 
+    private static int MAX_AVAILABLE_DEPTH = 30;
+
+    void recParse(Transform jointTransform, Dictionary<string, Vector3> joints, int depth)
+    {
+        if (depth > PoseEstimation.MAX_AVAILABLE_DEPTH)
+        {
+            return;
+        }
+        if (joints.ContainsKey(jointTransform.name))
+        {
+            jointTransform.position = joints[jointTransform.name];
+        }
+
+        for (int i = 0; i < jointTransform.childCount; i++)
+        {
+            recParse(jointTransform.GetChild(i), joints, depth + 1);
+        }
+    }
+
+    static Dictionary<string, Vector3> adjustInvalidJoints(Dictionary<string, Vector3> joints)
+    {
+        List<string> toBeRemoved = new List<string>();
+        foreach (var keyValue in joints)
+        {
+            if( !  joints.ContainsKey(PoseEstimation.getJointSuperior(keyValue.Key))  )
+                continue;
+            
+            if ( keyValue.Value.y > joints[PoseEstimation.getJointSuperior(keyValue.Key)].y)
+            {
+                toBeRemoved.Add(keyValue.Key);
+                // joints.Remove(keyValue.Key);
+            }
+        }
+
+        foreach (var jointName in toBeRemoved)
+        {
+            joints.Remove(jointName);
+        } 
+
+        return joints;
+    }
+
+    static string jointsToString(Dictionary<string, Vector3> joints)
+    {
+        string str = string.Empty;
+
+        foreach (var kv in joints)
+        {
+            str += "n=" + kv.Key + "p=" + kv.Value.ToString() + ";";
+        }
+
+        return str;
+    }
+    
+    void updateMesh(Dictionary<string, Vector3> joints)
+    {
+        GameObject p = this.clothMesh;
+        PoseEstimation.adjustInvalidJoints(joints);
+        Debug.Log(PoseEstimation.jointsToString(joints));
+        for (int i = 0; i < p.transform.childCount; i++)
+        {
+            if (p.transform.GetChild(i).name == "body")
+            {
+                recParse(p.transform.GetChild(i), joints, 1);
+            }
+        }
+    }
+
+    private class AxisInverter
+    {
+        private float maxSize;
+        private float minSize;
+
+        public AxisInverter(float min, float max)
+        {
+            this.minSize = min;
+            this.maxSize = max;
+        }
+
+        public float invert(float pos)
+        {
+            return this.maxSize - pos + this.minSize;
+        }
+    }
+    
+    private class Normalizer
+    {
+        private float min;
+        private float max;
+        private float returnStep;
+
+        public Normalizer(float min, float max, float returnStep = 1.0f)
+        {
+            this.min = min;
+            this.max = max;
+            this.returnStep = returnStep;
+        }
+
+        public float normalize(float val)
+        {
+            return (val - this.min) / (this.max - this.min) * returnStep;
+        }
+    }
+
     IEnumerator PoseUpdate()
     {
+        Normalizer xNorm = new Normalizer(0, this.Width, Screen.currentResolution.width);
+        Normalizer yNorm = new Normalizer(0, this.Height, Screen.currentResolution.height);
+
+        AxisInverter yAxisInv = new AxisInverter(0, this.Width);
+        
         if (texture != null)
         {
             Texture2D local_texture = scaled(texture, ImageSize, ImageSize);
@@ -83,18 +228,58 @@ public class PoseEstimation : MonoBehaviour
             local_texture = null;
             Resources.UnloadUnusedAssets();
 
+            string poseStr = string.Empty;
+
+            var jointsDict = new Dictionary<string, Vector3>();
+
             if (poses.Count() > 0)
             {
+                Tuple<float, float> leftEyePos = null;
+                Tuple<float, float> rightEyePos = null;
+                
                 PoseNet.Pose detected_pose = poses.Aggregate((maximum, current) => maximum.score > current.score ? maximum : current);
                 foreach (PoseNet.Keypoint keypoint in detected_pose.keypoints)
                 {
-                    Debug.Log(keypoint.part);
-                    Debug.Log(keypoint.position[0]);
-                    Debug.Log(keypoint.position[1]);
+                    var name = keypoint.part;
+                    var x = keypoint.position[0];
+                    var y = keypoint.position[1];
+
+                    if (name == "leftWrist")
+                    {
+                        Debug.Log(xNorm.normalize(x).ToString() + ", " + yNorm.normalize(y).ToString());
+                    }
+
+                    var xCam = this.camera.ScreenToWorldPoint(new Vector3(xNorm.normalize(x), yNorm.normalize(yAxisInv.invert(y)), camera.nearClipPlane + 0.5f));
+                 
+                    if(name == "leftEar" || name == "rightEar")
+                        continue;
+
+                    if (name == "leftEye")
+                        leftEyePos = new Tuple<float, float>(x, y);
+
+                    if (name == "rightEye")
+                        rightEyePos = new Tuple<float, float>(x, y);
+                    
+                    jointsDict.Add(name, xCam);
+                    
                 }
-                Debug.Log("");
+
+                if (leftEyePos != null && rightEyePos != null)
+                {
+                    // Debug.Log(Screen.currentResolution.ToString());
+                    // Debug.Log(leftEyePos.Item1.ToString() + ", " + rightEyePos.Item1.ToString());
+                    
+                    float xHead = (xNorm.normalize(leftEyePos.Item1) + xNorm.normalize(rightEyePos.Item1)) / 2;
+                    float yHead = (yNorm.normalize(yAxisInv.invert(leftEyePos.Item2)) + yNorm.normalize(yAxisInv.invert(rightEyePos.Item2))) / 2 - 150f;
+
+                    // Debug.Log(xHead.ToString() + ", " + yHead.ToString());
+
+                    jointsDict.Add("body", this.camera.ScreenToWorldPoint(new Vector3(xHead, yHead, camera.nearClipPlane + 0.5f)));
+                }
             }
+            this.updateMesh(jointsDict);
         }
+
 
         yield return new WaitForSeconds(3);
 
